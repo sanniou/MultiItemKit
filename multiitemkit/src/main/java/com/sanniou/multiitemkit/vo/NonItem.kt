959c284555fd8f95cdd154fd394a49304f77673b
yujichang <jichang.yu@1step.ai>
2020-01-15 18:27:27 +0800

    docs: update doc

--- multiitemkit/src/main/java/com/sanniou/multiitemkit/vo/NonItem.kt
+++ multiitemkit/src/main/java/com/sanniou/multiitemkit/vo/NonItem.kt
@@ -3,6 +3,11 @@ package com.sanniou.multiitemkit.vo
 import com.sanniou.multiitem.DataItem
 import com.sanniou.multiitemkit.R
 
+/**
+ * 用于recycler list 中删除某个item 而不想影响其他 index 硬编码的部分时，不占用任何显示空间
+ *
+ */
+
 class NonItem : DataItem {
     override fun getItemType() = R.layout.multiitemkit_item_non
 }
