0662ca8644da88e0bf6af173adbe87b67e1c843f
yujichang <jichang.yu@1step.ai>
2020-01-15 18:26:58 +0800

    feat: add new NoneItem

---
+++ multiitemkit/src/main/java/com/sanniou/multiitemkit/vo/NonItem.kt
@@ -0,0 +1,8 @@
+package com.sanniou.multiitemkit.vo
+
+import com.sanniou.multiitem.DataItem
+import com.sanniou.multiitemkit.R
+
+class NonItem : DataItem {
+    override fun getItemType() = R.layout.multiitemkit_item_non
+}
